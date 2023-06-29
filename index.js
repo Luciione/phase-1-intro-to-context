5605763530f8c029646e21e2406efde3f180f738
+ <lucy.njoroge1@student.moringaschool.com>
2023-06-30 00:16:46 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Your code here
+function createEmployeeRecord(employeeData) {
+    return {
+      firstName: employeeData[0],
+      familyName: employeeData[1],
+      title: employeeData[2],
+      payPerHour: employeeData[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+  }
+  
+  function createEmployeeRecords(employeesData) {
+    return employeesData.map(createEmployeeRecord);
+  }
+  
+  function createTimeInEvent(employee, timeStamp) {
+    const [date, hour] = timeStamp.split(" ");
+    employee.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour),
+      date: date
+    });
+    return employee;
+  }
+  
+  function createTimeOutEvent(employee, timeStamp) {
+    const [date, hour] = timeStamp.split(" ");
+    employee.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour),
+      date: date
+    });
+    return employee;
+  }
+  
+  function hoursWorkedOnDate(employee, date) {
+    const timeInEvent = employee.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = employee.timeOutEvents.find(event => event.date === date);
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
+  }
+  
+  function wagesEarnedOnDate(employee, date) {
+    const hoursWorked = hoursWorkedOnDate(employee, date);
+    return hoursWorked * employee.payPerHour;
+  }
+  
+  function allWagesFor(employee) {
+    const datesWorked = employee.timeInEvents.map(event => event.date);
+    return datesWorked.reduce((totalWages, date) => totalWages + wagesEarnedOnDate(employee, date), 0);
+  }
+  
+  function calculatePayroll(employees) {
+    return employees.reduce((totalPayroll, employee) => totalPayroll + allWagesFor(employee), 0);
+  }
+
+  // Test data
+const employeesData = [
+    ["John", "Doe", "Manager", 25],
+    ["Jane", "Smith", "Assistant", 20],
+  ];
+  
+  const employees = createEmployeeRecords(employeesData);
+  
+  // Perform operations and display results
+  console.log(calculatePayroll(employees));
+  
